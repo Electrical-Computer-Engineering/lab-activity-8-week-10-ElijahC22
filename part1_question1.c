1c84ab327726cf842d57c85d315814cfe0f23ee3
ElijahC22 <elijah.cole@stonybrook.edu>
2024-04-04 17:19:48 -0400

    LAB08UNF

--- part1_question1.c
+++ part1_question1.c
@@ -1,1 +1,98 @@
+#include <stdio.h>
+
+int my_isalpha(char c) {
+	return (c >= 'a' && c <= 'z' || c >= 'A' && c <= 'Z');
+}
+int my_isdigit(char c) {
+	return c >= '0' && c <= '9';
+}
+int my_isupper(char c) {
+	return c >= 'A' && c <= 'Z';
+}
+int my_toupper(char c) {
+	if (c >= 'a' && c <= 'z')
+		return c - 'a' + 'A';
+	return c;
+}
+int my_tolower(char c) {
+	if (c >= 'A' && c <= 'Z')
+		return c - 'A' + 'a';
+	return c;
+}
+int main() {
+	char c;
+	char d;
+	int i;
+	int count = 2;
+	
+	for (i = 0; i < count; i++){
+		printf("Enter alphabetic character: ");
+		scanf("%c", &c); getchar();
+		int d = my_isalpha(c);		//use function to determin if alpha
+		if(d == 1){
+			printf("This is an alphabet character\n");
+		}
+		else{
+			printf("This is not an alphabetic character\n");
+		}
+	}
+	printf("\n");
+	
+	for (i = 0; i < count; i++){
+		printf("Enter a digit: ");
+		scanf("%c", &c);getchar();
+		int d = my_isdigit(c);		//use function to determin if digit
+		if(d == 1){
+			printf("This is a digit\n");
+		}
+		else{
+			printf("This is not a digit\n");
+		}
+	}
+	
+	printf("\n");
+	for (i = 0; i < count; i++){
+		printf("Enter an uppercase character: ");
+		scanf("%c", &c); getchar();
+		int d = my_isupper(c);		//use function to determin uppercase
+		if(d == 1){
+			printf("This is an uppercase character\n");
+		}
+		else{
+			printf("This is not an uppercase character\n");
+		}
+	}
+	
+	printf("\n");
+	for (i = 0; i < count; i++){
+		printf("Enter lowercase alphabetic character to make uppercase: ");
+		scanf("%c", &c);getchar();
+		int d = my_toupper(c);		//use function to make upper if lower
+		if(c >= 'a' && c <= 'z'){
+			printf("Your new character is %c\n", d);
+		}
+		else{
+			printf("%c is already uppercase\n", c);
+		}
+	}
+	
+	printf("\n");
+	for (i = 0; i < count; i++){
+		printf("Enter uppercase alphabetic character to make lowercase: ");
+		scanf("%c", &c);getchar();
+		int d = my_tolower(c);		//use function to make uppercase lowercase
+		if(c >= 'A' && c <= 'Z'){
+			printf("Your new character is %c\n", d);
+		}
+		else{
+			printf("%c is already lowercase\n", c);
+		}
+	}
+	
+	
+	
+}
+
+
+
 
--- part2_question1.c
+++ part2_question1.c
@@ -1,1 +1,63 @@
+#include <stdio.h>
+
+int i;
+int my_strcmp(char s1[], char s2[]) {
+		for(i = 0; s1[i] != '\0' && s2[i] != '\0'; i++){	//seeing which string is larger by stopping at the \0
+	return s1[i] != s2[i];		//if they are not equal 1, if they are equal 0
+	}
+}
+
+int my_strncmp(char s1[], char s2[],int n) {
+		for(i = 0; s1[i] != "%c" && s2[i] != "%c"; i++){	//seeing which string is larger by stopping at n, however many characters the user wants
+	return s1[i] != s2[i];		//if they are not equal 1, if they are equal 0
+	}
+}
+
+int my_strcpy(char s1[], char s2[]) {
+		for(i = 0; s1[i] != '\0' && s2[i] != '\0'; i++){	//seeing which string is larger by stopping at the \0
+			temp[i] = s1[i];		//these two lines copy s2 into s1
+			s1[i] = s2[i];
+	}
+}
+
+int my_strcat(char s1[], char s2[]) {
+		for(i = 0; s1[i] != '\0' && s2[i] != '\0'; i++){	//seeing which string is larger by stopping at the \0
+			s1[i] += s2[i];		//add s2 to s1
+	}
+}
+
+int my_reverse(char s1[], char s2[]) {
+		for(i = 0; s1[i] != '\0' && s2[i] != '\0'; i++){	//seeing which string is larger by stopping at the \0
+		for(i = 0; i < s2[i] / 2; i++){		//reverse the second string
+			temp[i] = s1[i];		//inputing reversed second string into first string
+			s1[i] = s2[i];
+		}
+	}
+}
+#define MAX 32
+int main(){
+	char s1[MAX] = "HI ESE";
+	char s2[MAX] = "BYE ESE";
+	char d;
+	int count = 2;
+	
+	for (i = 0; i < count; i++){
+		
+		int d = my_strcmp(s1, s2);		//use function to determin if alpha
+		if(d == 1){
+			printf("These strings are not equal");
+		}
+		else{
+			printf("These strings are equal\n");
+		}
+	}
+		
+		
+		
+	}
+	
+	
+	
+	
+}
 
